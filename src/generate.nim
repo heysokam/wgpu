fe6905ad1bd4845f994ef658e426daec22d6de5a
sOkam <7308253+heysokam@users.noreply.github.com>
2024-03-18 10:13:37 +0100

    bld: Add Generator buildsystem

---
+++ src/generate.nim
@@ -0,0 +1,50 @@
+# @deps std
+from std/strformat import `&`
+# @deps ndk
+import confy
+
+info "Generating wgpu-native bindings ..."
+
+#_______________________________________
+# Configure the Generator Buildsystem
+confy.cfg.nim.systemBin = off
+let genDir  = cfg.rootDir/"gen"
+let genFile = genDir/"result.nim"
+let srcDir  = cfg.srcDir
+let trgFile = build.srcDir/"wgpu"/"api.nim"
+
+#_______________________________________
+# Install Opir if it doesn't exist
+import confy/builder/nim as cc
+let opir = getEnv("HOME").Path/".local"/"bin"/"opir"
+if not fileExists(opir):
+  info &"Opir does not exist at {opir.parentDir()}"
+  info "Installing Futhark and Opir ..."
+  withDir libDir/"futhark": sh &"{cc.getRealNimble()} install"
+  ln getEnv("HOME").Path/".nimble"/"bin"/"opir", opir
+  info "Done installing Futhark and Opir."
+
+#_______________________________________
+# Build the generator
+info "Generating wgpu-native bindings with Futhark ..."
+cfg.srcDir = genDir
+build Program.new(
+  src  = cfg.srcDir/"generator.nim",
+  deps = Dependencies.new(
+    submodule( "futhark",      "https://github.com/PMunch/futhark"      ),
+    submodule( "libclang-nim", "https://github.com/PMunch/libclang-nim" ),
+    submodule( "termstyle",    "https://github.com/PMunch/termstyle"    ),
+    submodule( "macroutils",   "https://github.com/PMunch/macroutils"   ),
+    submodule( "nimbleutils",  "https://github.com/PMunch/nimbleutils"  ),
+    ), # << Dependencies.new( ... )
+  args = "--maxLoopIterationsVM:100_000_000 -d:futharkRebuild -d:nodeclguards",
+  ) # << Program.new( ... )
+info "Done generating wgpu-native bindings with Futhark."
+
+#_______________________________________
+# Move the generated output
+cfg.srcDir = build.srcDir
+info &"Outputting the resulting bindings into:  {trgFile}"
+cp genFile, trgFile
+info "Done generating wgpu-native bindings."
+
